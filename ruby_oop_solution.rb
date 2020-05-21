967aeedab1ab8d10b85906c5cf7cde4195a71c4d
Becca <rrdoukas@gmail.com>
2020-05-21 17:02:29 -0500

    first portion of homework

--- ruby_oop_solution.rb
+++ ruby_oop_solution.rb
@@ -1,38 +1,41 @@
 # #Write your solution below:
 
-# class Animal 
-#   attr_writer :state
-#   attr_reader :state
+class Animal 
+  attr_writer :state
+  attr_reader :state
 
-#   def initialize(type)
-#     @type = type
-#   end 
+  def initialize(type)
+    @type = type
+  end 
 
-#   def eat(food)
-#     puts "The #{@type} is eating #{food}."
-#   end 
+  def eat(food)
+    if food == "person"
+      food = "nothing"
+    puts "The #{@type} is eating #{food}."
+    end
+  end 
 
-#   def sleep
-#     @state = "sleep"
-#   end 
+  def sleep
+    @state = "sleep"
+  end 
 
-#   def awake
-#     @state = "awake"
-#   end 
+  def awake
+    @state = "awake"
+  end 
 
-# end 
+end 
 
-# tiger = Animal.new("tiger")
-# elephant = Animal.new("elephant")
-# mouse = Animal.new("mouse")
+tiger = Animal.new("tiger")
+elephant = Animal.new("elephant")
+mouse = Animal.new("mouse")
 
-# tiger.awake
-# elephant.sleep
-# mouse.awake
+tiger.awake
+elephant.sleep
+mouse.awake
 
-# p tiger
-# p elephant
-# p mouse
+p tiger
+p elephant
+p mouse
 
 
 
@@ -41,8 +44,22 @@ class Person < Animal
   def initialize(input_options)
     super 
     @type = "Person"
+    @age = input_options[:age]
+    @gender = input_options[:gender]
   end 
 
-  
+  def eat(food)
+    if food == "person"
+      food = "nothing"
+      puts "They are eating #{food}."
+    end
+  end 
+end
 
-end 
+
+person1 = Person.new(type: "person", age: 10, gender:"female", state: "awake")
+
+person1.eat("cheese")
+puts person1.eat("person")
+
+p person1
